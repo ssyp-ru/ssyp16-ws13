2b71b4cbdb6c58f9b3ec6b0986d189c2251f2c5f
Artyom Malygin <temahome2012@yandex.ru>
2016-07-23 16:00:17 +0600

    Add tests which testing methods by class FSImplementation

--- test/fs.test.ts
+++ test/fs.test.ts
@@ -2,9 +2,96 @@ import * as nfs from 'fs';
 import * as path from 'path';
 import * as FS from '../fs';
 import * as assert from 'assert';
+var createHash = require('sha.js');
 
 describe("File System", () => {
-    it("", () => {
+    describe("Class FSImplementation implements IFileSystem", () => {
+        it(".create", () => {
+            var fs = FS.fs();
+            var bf1 = new Buffer("Hello");
+            var firstFileObject = fs.create(bf1);
+            var firstHash = createHash('sha256').update("Hello").digest('hex');
+            nfs.writeFileSync('.jerk/' + firstHash, bf1, { mode: 0o655 });
+            assert.deepEqual(nfs.readFileSync('.jerk/' + firstHash, 'utf-8'), firstFileObject);
+            /*var bf2 = new Buffer("World");
+            var secondFileObject = fs.create(bf2);
+            var secondHash = createHash('sha256').update("World").digest('hex');
+            nfs.writeFileSync('.jerk/' + secondHash, bf2, { mode: 0o655 });
+            assert.deepEqual(nfs.readFileSync('.jerk/' + secondHash, 'utf-8'), secondFileObject);*/
+        });
 
+        it(".symlink", () => {
+            var fs = FS.fs();
+            var path1 = "Hello";
+            var firstSymlinkObject = fs.symlink(path1);
+            var firstHash = createHash('sha256').update("Hello").digest('hex');
+            nfs.writeFileSync('.jerk/' + firstHash, path1, { mode: 0o655 });
+            assert.deepEqual(nfs.readFileSync('.jerk/' + firstHash, 'utf-8'), firstSymlinkObject);
+            /*var path2 = new String("World");
+            var secondFileSymlink = fs.symlink(path2);
+            var secondHash = createHash('sha256').update("World").digest('hex');
+            nfs.writeFileSync('.jerk/' + secondHash, path2, { mode: 0o655 });
+            assert.deepEqual(nfs.readFileSync('.jerk/' + secondHash, 'utf-8'), secondSymlinkObject);*/
+        });
+
+
+        it(".resolveObjectByHash", () => {
+            var fs = FS.fs();
+            var bf1 = new Buffer("Hello");
+            var firstFileObject = fs.create(bf1);
+            var firstHash = createHash('sha256').update("Hello").digest('hex');
+            nfs.writeFileSync('.jerk/' + firstHash, bf1, { mode: 0o655 });
+            assert.deepEqual(nfs.readFileSync('.jerk/' + firstHash, 'utf-8'), fs.resolveObjectByHash(firstHash));
+            /*var bf2 = new Buffer("World");
+            var secondFileObject = fs.create(bf2);
+            var secondHash = createHash('sha256').update("World").digest('hex');
+            nfs.writeFileSync('.jerk/' + secondHash, bf2, { mode: 0o655 });
+            assert.deepEqual(nfs.readFileSync('.jerk/' + secondHash, 'utf-8'), fs.resolveObjectByHash(secondHash));*/
+        });
+
+        it(".resolveObjectByContents", () => {
+            var fs = FS.fs();
+            var bf1 = new Buffer("Hello");
+            var firstFileObject = fs.create(bf1);
+            var firstHash = createHash('sha256').update("Hello").digest('hex');
+            nfs.writeFileSync('.jerk/' + firstHash, bf1, { mode: 0o655 });
+            assert.deepEqual(nfs.readFileSync('.jerk/' + firstHash, 'utf-8'), fs.resolveObjectByContents(bf1));
+            /*var bf2 = new Buffer("World");
+            var secondFileObject = fs.create(bf2);
+            var secondHash = createHash('sha256').update("World").digest('hex');
+            nfs.writeFileSync('.jerk/' + secondHash, bf2, { mode: 0o655 });
+            assert.deepEqual(nfs.readFileSync('.jerk/' + secondHash, 'utf-8'), fs.resolveObjectByContents(bf2));*/
+        });
+
+        it(".entries", () => {
+            var fs = FS.fs();
+            var bf1 = new Buffer("Hello");
+            var firstFileObject = fs.create(bf1);
+            var firstHash = createHash('sha256').update("Hello").digest('hex');
+            nfs.writeFileSync('.jerk/' + firstHash, bf1, { mode: 0o655 });
+            var bf2 = new Buffer("World");
+            var secondFileObject = fs.create(bf2);
+            var secondHash = createHash('sha256').update("World").digest('hex');
+            nfs.writeFileSync('.jerk/' + secondHash, bf2, { mode: 0o655 });
+            assert.deepEqual(nfs.readdirSync('.jerk').filter(v => v.length > 60).map(v => fs.resolveObjectByHash(v)), fs.entries());
+        });
+
+        it(".remove", () => {
+            var fs = FS.fs();
+            var bf1 = new Buffer("Hello");
+            var firstFileObject = fs.create(bf1);
+            var firstHash = createHash('sha256').update("Hello").digest('hex');
+            var bf2 = new Buffer("World");
+            var secondFileObject = fs.create(bf2);
+            var secondHash = createHash('sha256').update("World").digest('hex');
+            /*assert.doesNotThrow(() => fs.remove(firstHash));
+            assert.deepEqual(nfs.readFileSync('.jerk/' + firstHash, 'utf-8'), undefined);
+            assert.deepEqual(nfs.readFileSync('.jerk/' + secondHash, 'utf-8'), secondFileObject);
+            assert.throws(() => fs.remove(firstHash), /err/);*/
+            assert.equal(fs.remove(firstHash), true);
+            assert.deepEqual(nfs.readFileSync('.jerk/' + firstHash, 'utf-8'), undefined);
+            assert.deepEqual(nfs.readFileSync('.jerk/' + secondHash, 'utf-8'), secondFileObject);
+            assert.equal(fs.remove(firstHash), false);
+        });
     });
 });
