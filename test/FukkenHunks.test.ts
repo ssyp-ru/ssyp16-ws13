e098db1ccbbeb3754712903be4f4d03e6ecdbcbb
Artyom Malygin <temahome2012@yandex.ru>
2016-07-25 21:26:20 +0600

    [DON'T STABILITY] Add 2 don't work tests(.diffFiles by Malygin and .merge by Redko)

--- test/FukkenHunks.test.ts
+++ test/FukkenHunks.test.ts
@@ -57,6 +57,54 @@ describe("Hulk module", () => {
             assert.deepEqual(hunks, diff.hunks);
         });
 
-        it("");
+        it(".diffFiles", () => {
+            var fs = FS.fs();
+            var bf1 = new Buffer("Hello, Artyom! Jerk commit!");
+            var firstFileObject = fs.create(bf1);
+            var firstHash = createHash('sha256').update(bf1).digest('hex');
+            nfs.writeFileSync('.jerk/' + firstHash, bf1, { mode: 0o644 });
+            var bf2 = new Buffer("Hello, World! Jerk push!");
+            var secondFileObject = fs.create(bf1);
+            var secondHash = createHash('sha256').update(bf2).digest('hex');
+            nfs.writeFileSync('.jerk/' + secondHash, bf2, { mode: 0o644 });
+            var hunks: HunkExp.Hunk[] = [];
+            var diff = new HunkExp.Diff(hunks);
+            var words: Buffer[] = [];
+            words[0] = new Buffer("Artyom");
+            words[1] = new Buffer("World");
+            words[2] = new Buffer("commit");
+            words[3] = new Buffer("push");
+            hunks.push(new HunkExp.Hunk(words[0], words[1]));
+            hunks.push(new HunkExp.Hunk(words[2], words[3]));
+            assert.deepEqual(HunkExp.Diff.diffFiles(firstFileObject, secondFileObject), new HunkExp.Diff(hunks));
+        });
+
+        it(".merge", () => {
+            var fs = FS.fs();
+            var bf1 = new Buffer("Hello, Artyom! Jerk commit!");
+            var firstFileObject = fs.create(bf1);
+            var firstHash = createHash('sha256').update(bf1).digest('hex');
+            nfs.writeFileSync('.jerk/' + firstHash, bf1, { mode: 0o644 });
+            var bf2 = new Buffer("Hello, World! Jerk push!");
+            var secondFileObject = fs.create(bf1);
+            var secondHash = createHash('sha256').update(bf2).digest('hex');
+            nfs.writeFileSync('.jerk/' + secondHash, bf2, { mode: 0o644 });
+            var hunks: HunkExp.Hunk[] = [];
+            var diff = new HunkExp.Diff(hunks);
+            var words: Buffer[] = [];
+            words[0] = new Buffer("Artyom");
+            words[1] = new Buffer("World");
+            words[2] = new Buffer("commit");
+            words[3] = new Buffer("push");
+            
+            assert.deepEqual(HunkExp.Diff.merge());
+        });
+
+        it("checks MergeConflict", () => {
+            var Diff1 = new Hulk.Diff(fs0, fs1);
+            var Diff2 = new HunkExp.Diff(fs1, fs2);
+            assert.ok(typeof(Hulk.Diff.merge(Diff1,Diff2)) === typeof(Hulk.MergeConflict))
+            assert.equal(Hulk.Diff.merge(Diff1, Diff2)[0].base, Diff1.hunks[0]);
+        })
     });
 });
