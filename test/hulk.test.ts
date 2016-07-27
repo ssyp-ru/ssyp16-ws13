93e51f1eb9838e5c4e08b0486f789ea844f7d0a9
Sergey Redko <sergey@redko.me>
2016-07-27 16:03:10 +0600

    Trying to push

---
+++ test/hulk.test.ts
@@ -0,0 +1,31 @@
+/*import * as assert from 'assert'
+import * as Hulk from '../hulk'
+
+describe("Hulk", () => {
+    describe("#diff", () => {
+        it("returns null on equal buffers", () => {
+            var buffer = new Buffer("a\nb\nc\nd\n");
+            var diff = Hulk.Diff.diffFiles(buffer, buffer);
+            assert.equal(diff, null);
+        });
+        it("correctly removes strings when they are not needed", () => {
+            var first = "a\nb\nc\nd\n", second = "a\nb\nc\n";
+            var firstBuffer = new Buffer(first), secondBuffer = new Buffer(second);
+            var diff = Hulk.Diff.diff(firstBuffer, secondBuffer);
+            assert.ok(diff);
+            assert.equal(diff.hunks[0].type, Hulk.HunkOperation.Remove);
+            assert.equal(diff.hunks[0].line, 3);
+        });
+        it("returns null on 2 empty files", () => {
+            var first = "", second = "";
+            var diff = Hulk.Diff.diff(new Buffer(first), new Buffer(second));
+            assert.ok(!diff);
+        });
+        it("changes one string to another", () => {
+            assert.fail();
+        });
+        it("adds strings when they are provided in new file", () => {
+            assert.fail();
+        });
+    });
+});*/
